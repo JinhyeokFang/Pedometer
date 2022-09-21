15c1b1b1cb36ac548e9aa3649d9201d8a76911cf
JinhyeokFang <jinhyeokfang@gmail.com>
2022-09-22 00:01:19 +0900

    init

---
+++ pages/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card>
+        <v-card-title class="headline">
+          Pedometer
+        </v-card-title>
+        <v-card-text>
+          Pedometer는 Github README.md 방문자 수를 보여주고 통계를 제공하는 서비스입니다.
+        </v-card-text>
+        <v-text-field
+          label="URL을 입력해주세요."
+          class="mx-4"
+          v-model="url"
+        ></v-text-field>
+        <v-card-text>
+          {{ pedometerUrl }}
+        </v-card-text>
+        <v-card-actions>
+            <v-btn
+              nuxt
+              :to="statisticPageURL"
+            >
+              통계 보기
+            </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  data() {
+    return {
+      url: ''
+    }
+  },
+  computed: {
+    pedometerUrl() {
+      if (this.url == '')
+        return 'URL을 입력해주세요.'
+      return `https://f9039c3zbh.execute-api.us-east-1.amazonaws.com/dev/count/${Buffer.from(this.url).toString('base64')}/image.svg`;
+    },
+    statisticPageURL() {
+      return '/statistic/' + Buffer.from(this.url).toString('base64');
+    }
+  }
+}
+</script>
---
+++ pages/statistic/_url.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card>
+        <v-card-title class="headline">
+          {{ decodedURL }}
+        </v-card-title>
+        <canvas id="myChart" style="width:100%;max-width:600px"></canvas>
+        <v-card-text v-for="item of visitData" :key="item.name">
+          {{ item }}
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import dayjs from 'dayjs'
+import Chart from 'chart.js/auto'
+
+export default {
+  name: 'StatisticPage',
+  data() {
+    return {
+      visitData: []
+    }
+  },
+  computed: {
+    decodedURL() {
+      return Buffer.from(this.$route.params.url, 'base64').toString();
+    }
+  },
+  async mounted() {
+    const result = await this.$axios.get(`https://f9039c3zbh.execute-api.us-east-1.amazonaws.com/dev/count/${this.$route.params.url}/all`);
+    const lists = JSON.parse(Buffer.from(result.data, 'base64').toString());
+    for (let rawData of lists) {
+      const date = dayjs(rawData.createdAt);
+      const dataIndex = this.visitData.findIndex(item => item.name == `${date.year()}-${date.month()}-${date.date()}`);
+      if (dataIndex == -1) {
+        this.visitData.push({
+          name: `${date.year()}-${date.month()}-${date.date()}`,
+          value: 1
+        });
+        continue;
+      }
+      this.visitData[dataIndex].value += 1;
+    }
+
+    var xValues = ["title"];
+    var yValues = [100];
+    var barColors = ["blue"];
+
+    new Chart("myChart", {
+      type: "bar",
+      data: {
+        labels: xValues,
+        datasets: [{
+          backgroundColor: barColors,
+          data: yValues
+        }]
+      },
+      options: {
+        legend: {display: false},
+        title: {
+          display: true,
+          text: "title"
+        }
+      }
+    });
+  }
+}
+</script>
